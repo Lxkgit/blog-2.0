4f32c4e78757c8d1f3e2b26a25c144bb4e913bd1
silengzi <474960201@qq.com>
2022-06-21 08:54:00 +0800

    init vue

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { useStore } from 'vuex';
+
+defineProps<{ msg: string }>()
+
+const store = useStore()
+
+const addCount = () => {
+  store.commit('increment')
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p>vuex test: {{ store.state.count }}</p>
+  <button @click="addCount">点我count+1</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
