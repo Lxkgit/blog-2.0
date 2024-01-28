2e4ecee58386b27a9e72bd41d1538ec74e215cb7
lxk <470687917@qq.com>
2024-01-28 09:52:06 +0800

    24-01-28【功能新增】：完成监听mqtt消息，控制舵机旋转角度

--- arduino/d1/wifi_mqtt_demo/wifi_mqtt_demo.ino
+++ arduino/d1/wifi_mqtt_demo/wifi_mqtt_demo.ino
@@ -1,39 +1,44 @@
-#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <Servo.h> // 舵机控制
+#include <DHT.h> // 温湿度传感器
+#include <ArduinoJson.h>
+#include <stdio.h>
+#include <string.h>
 
-const char* ssid = "TP-LINK_424D";       //“wifi热点名称”
-const char* passwd = (char*)"tplink96012";  //”wifi热点密码”
-const char* mqtt_server = "192.168.0.106"; // 服务器地址
-const int port = 1883;//服务器端口号
-const char* topic_name = "test";
-const char*client_id = "wemos001";//这个是板子的编号  我没有懂 但是不影响使用
+const char* ssid = "TP-LINK_424D";       // wifi热点名称
+const char* passwd = (char*)"tplink96012";  // wifi热点密码
+const char* mqtt_server = "192.168.0.106"; // mqtt服务器地址
+const int port = 1883; // mqtt服务器端口号
+const char* MESSAGE_TOPIC = "SENSOR_DATA"; // 发送消息mqtt topic
+const char* CONTROL_TOPIC = "SENSOR_CONTROL";
+const char* client_id = "WeMosD1-01"; //这个是板子的编号
 
-//定义针脚
-#define DHTPIN 4            //定义类型，DHT11或者其它
-#define DHTTYPE DHT11        //进行初始设置 
+//定义
+#define DuoPIN D5   // 定义D5管脚为舵机控制引脚
+#define DHTPIN D4            // 定义D5管脚为温湿度传感器数据传入引脚
+#define DHTTYPE DHT11        // 指定传感器类型
+
+
 DHT dht(DHTPIN, DHTTYPE);
-
-unsigned long lastSend;
+long lastSend;
 WiFiClient espclient;
 PubSubClient client(espclient);
+Servo myservo;  //创建一个舵机控制对象
 
 //获取传感器数据进行发送
-void gettemp()
-{
+void gettemp() {
   //上传的字符串进行拼接转义
   float h = dht.readHumidity();//读湿度
   float t = dht.readTemperature();//读温度，默认为摄氏度
 
   char attributes[100];
-  sprintf(attributes, "{\"h\":%.2f, \"t\":%.2f}", h, t); // 将num转换为字符串并赋值给str
-
-//  payload.toCharArray( attributes, 100 );
-  client.publish(topic_name, attributes );
+  sprintf(attributes, "{\"chipType\":\"%s\",\"sensorType\":\"DHT11\",\"data\":[{\"key\":\"温度\",\"value\":\"%.2f\"},{\"key\":\"湿度\",\"value\":\"%.2f\"}]}", client_id, t, h);
+  client.publish(MESSAGE_TOPIC, attributes );
 
 }
 //初始化wifi
-void setup_wifi() {
+void init_wifi() {
   delay(10);
   WiFi.begin(ssid, passwd);
   while (WiFi.status() != WL_CONNECTED) {
@@ -41,11 +46,14 @@ void setup_wifi() {
     Serial.print("...");
   }
 }
+
 //连接服务器
 void reconnect() {
   while (!client.connected()) { //如果没有连上
     if (client.connect(client_id)) { //接入时本设备id
       Serial.println("connect success");
+      // 连接成功时订阅主题
+      client.subscribe(CONTROL_TOPIC);
     } else {
       Serial.print("failed,rc=");
       Serial.print(client.state());
@@ -54,19 +62,49 @@ void reconnect() {
     }
   }
 }
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);   // 打印主题信息
+  Serial.print("]");
+  char controlData[255];
+  for (int i = 0; i < length; i++) {
+    controlData[i] = (char)payload[i];
+    Serial.print((char)payload[i]); // 打印主题内容
+  }
+  StaticJsonDocument<255> jsonBuffer; //声明一个JsonDocument对象，长度200
+  // 反序列化JSON
+  DeserializationError error = deserializeJson(jsonBuffer, controlData);
+  if (error)
+  {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+  // 解析JSON {"chipType":"WeMosD1-01","sensorType":"DUO","data":10}
+  const char* chipType = jsonBuffer["chipType"];
+  const char* sensorType = jsonBuffer["sensorType"];
+  long data1  = jsonBuffer["data"];
+  if (strcmp(chipType, client_id) == 0) {
+    if (strcmp(sensorType, "DUO") == 0) {
+      myservo.write(data1);
+    }
+  }
+}
+
 //设备初始化
 void setup() {
   Serial.begin(9600);
-  setup_wifi();
+  init_wifi();
   client.setServer(mqtt_server, port);
+  client.setCallback(callback);
   dht.begin();            //DHT开始工作
+  myservo.attach(DuoPIN);
   lastSend = 0;
 }
 void loop() {
   reconnect();  //1.首先进行服务器的连接  因为这是循环函数 所以执行一次后判断就行了
-  if (millis() - lastSend > 10000) {
-    gettemp();
-    lastSend = millis();
-  }
+  delay(1000);
+  gettemp();
   client.loop();
 }
