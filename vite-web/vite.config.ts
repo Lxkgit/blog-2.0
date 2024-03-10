ed08882d1c0b050e0ca5686ddf5dc72bdc2ff9b7
lxk <470687917@qq.com>
2024-03-11 00:08:12 +0800

    24-03-11【功能新增】：前端添加开发环境与生产环境的配置

--- vite-web/vite.config.ts
+++ vite-web/vite.config.ts
@@ -1,32 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from "path";
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve:{
-    alias: {
-      "@": resolve(__dirname, "src")
-    }
-  },
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    })
-  ],
-  server: {
-    proxy:{
-			'/api':{
-          // target: 'http://127.0.0.1:9527',	//实际请求地址
-          target: 'http://172.18.0.10:9527',	//实际请求地址
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(
+  ({ command, mode, ssrBuild }) => {
+    const env = loadEnv(mode, process.cwd());
+    const ip = env.VITE_BASE_API;
+    return {
+      resolve: {
+        alias: {
+          "@": resolve(__dirname, "src")
+        }
+      },
+      plugins: [
+        vue(),
+        Components({
+          resolvers: [ElementPlusResolver()],
+        })
+      ],
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://' + ip + ':9527',	//实际请求地址
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          }
+        },
       }
-		},
+    }
   }
-})
+)
